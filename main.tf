1306f52d53219889d5f8e600fa69da129c58902f
Akhil <akhil.ginuga999@gmail.com>
2022-10-13 21:35:20 -0400

    fmt fix

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 
-data  "aws_organizations_organization" "root" {}
+data "aws_organizations_organization" "root" {}
 resource "aws_organizations_organizational_unit" "Snowplow" {
   name      = "Snowplow"
   parent_id = data.aws_organizations_organization.root.roots[0].id
 }
 
 resource "aws_organizations_policy" "Admin_policy" {
-name = "Admin_policy"
+  name    = "Admin_policy"
   content = <<CONTENT
 {
   "Version": "2012-10-17",
