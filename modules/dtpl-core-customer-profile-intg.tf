27202aca58bf80307df6875a91a707adabff1341
Akhil <akhil.ginuga999@gmail.com>
2022-10-25 12:45:36 -0400

    Update dtpl-core-customer-profile-intg.tf

--- modules/dtpl-core-customer-profile-intg.tf
+++ modules/dtpl-core-customer-profile-intg.tf
@@ -12,7 +12,7 @@ module "dtpl-core-customer-profile-intg" {
 
   account_tags = {
     "OwnerName"   = "Kaloyan Kosev"
-    "OwnerEmail"  = "kaloyankoevpokerstarsint.com"
+    "OwnerEmail"  = "kaloyankoe&&&vpokerstarsint.com"
     "Division"    = "ENT"
     "Environment" = "intg"
     "CostCenter"  = "80716"
